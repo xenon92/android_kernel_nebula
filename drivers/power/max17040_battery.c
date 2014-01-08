9be1285e820b4865dfc36cac3356ff58a263c296
Shubhang <shubhang92@gmail.com>
2014-01-08 16:16:51 +0530

    Revert "Add Battery Life eXtender (BLX)"
    This reverts commit 9da0a5c4401eb4d7d4d1a89efc58610257967f3b.

--- drivers/power/max17040_battery.c
+++ drivers/power/max17040_battery.c
@@ -25,10 +25,6 @@
 #include <linux/broadcom/cmp_battery_multi.h>
 #endif
 
-#ifdef CONFIG_BLX
-#include <linux/blx.h>
-#endif
-
 #define MAX17040_VCELL_MSB	0x02
 #define MAX17040_VCELL_LSB	0x03
 #define MAX17040_SOC_MSB	0x04
@@ -188,11 +184,7 @@ static void max17040_get_status(struct i2c_client *client)
 		chip->status = POWER_SUPPLY_STATUS_DISCHARGING;
 	}
 
-	#ifdef CONFIG_BLX
-		if (chip->soc >= get_charginglimit())
-	#else
-		if (chip->soc > MAX17040_BATTERY_FULL)
-	#endif
+	if (chip->soc > MAX17040_BATTERY_FULL)
 		chip->status = POWER_SUPPLY_STATUS_FULL;
 }
 
