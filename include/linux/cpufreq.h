5309ea4c209cadfa2d19d09861f7f78365d5282b
Shubhang <shubhang92@gmail.com>
2014-04-29 15:43:18 +0530

    cpufreq: add SmartassH3 governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -367,6 +367,9 @@ extern struct cpufreq_governor cpufreq_gov_smartass2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVE)
 extern struct cpufreq_governor cpufreq_gov_lulzactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_lulzactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_smartassH3)
+extern struct cpufreq_governor cpufreq_gov_smartassH3;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartassH3)
 #endif
 
 
